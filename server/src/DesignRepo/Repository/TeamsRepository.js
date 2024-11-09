cc18b90071b9079a292bdebacc03d3b5bcd5efca
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-11-08 22:01:22 -0500

    creating pattern design (Teams repository) repository design

--- server/src/DesignRepo/Repository/TeamsRepository.js
+++ server/src/DesignRepo/Repository/TeamsRepository.js
@@ -1,1 +1,12 @@
+const { Teams } = require("../../models/Teams");
 
+class TeamsRepository{
+  async postTeam( teamsData ){
+    try {
+      return await Teams.create( teamsData );
+    } catch (error) {
+      console.error("Teams could not be created: ", error);
+      throw error;
+    }
+  }
+}
