638eb5705d4678541175e2c8c6a1279d028d5f79
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-11-12 21:00:07 -0500

    creating main content routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,10 @@
+const driversRoutes = require("./driversRoutes");
+const teamsRoutes = require("./teamsRoutes");
 const { Router } = require("express");
-
 const router = Router();
+
+router.route("/api")
+      .use("/drivers", driversRoutes)
+      .use("/teams", teamsRoutes);
 
 module.exports = router;
