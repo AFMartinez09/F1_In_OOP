ef8bf2f0ff85d0495b9e9ac980506b631fc9b317
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-11-19 22:01:30 -0500

    nesting routes to handlers drivers and teams (driversHandlers and teamsHandlers

--- server/src/routes/driversRoutes.js
+++ server/src/routes/driversRoutes.js
@@ -1,27 +1,15 @@
 const DriversHandlers = require("../handlers/driversHandlers");
-// const DriversService = require("../service/DriversService");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", DriversHandlers.getAllDrivers);
 
-router.post("/", DriversHandlers.postDriver);
+router.post("/", DriversHandlers.setPostDriver);
 
-router.ger("/name", DriversHandlers.getDriverByName);
+router.ger("/name", DriversHandlers.getDriversByName);
 
 router.route("/:id")
       .get(DriversHandlers.getDriverById)
-      .put(DriversHandlers.putDriver)
-      .delete(DriversHandlers.deleteDriver);
-
-// router.get("/", DriversService.getAllDrivers);
-
-// router.post("/", DriversService.postDriver);
-
-// router.ger("/name", DriversService.getDriverByName);
-
-// router.route("/:id")
-//       .get(DriversService.getDriverById)
-//       .put(DriversService.putDriver)
-//       .delete(DriversService.deleteDriver);
+      .put(DriversHandlers.setPutDriver)
+      .delete(DriversHandlers.setDeleteDriver);
 
--- server/src/routes/teamsRoutes.js
+++ server/src/routes/teamsRoutes.js
@@ -1,7 +1,5 @@
 const TeamsHandlers = require("../handlers/teamsHandlers");
-// const TeamsService = require("../service/TeamsService");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", TeamsHandlers.getTeams)
-// router.get("/", TeamsService.getTeams);
