2c10faaece2d913e82958351bfa504a356ea2075
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-11-19 21:59:19 -0500

    creating service teams to use respository pattern

---
+++ server/src/service/TeamsService.js
@@ -0,0 +1,51 @@
+const { Teams } = require("../db");
+const axios = require("axios");
+
+class TeamsService {
+  static URL = process.env.URL;
+
+  async getTeams(){
+    try {
+      const dbTeams = await Teams.findAll({
+        include: {
+          model: Teams,
+          attributes: ["name"],
+        }
+      });
+      if(dbTeams.length === 0){
+        try {
+          const { data } = await axios.get(`${TeamsController.URL}`);
+          if(!data) throw new Error("Teams were not found");
+
+          const allTeams = [];
+
+          data.forEach((driver) => {
+            const { teams } = driver;
+            if(teams){
+              const splitTeams = teams.split(',').map(teams => teams.trim());
+              allTeams.push(...splitTeams);
+            }
+          });
+
+          const removeDuplicate = new Set(allTeams);
+          const orderArray = Array.from(removeDuplicate).sort();
+
+          const driverTeams = orderArray.map((team) => {
+            return { name: team };
+          });
+
+          const teams = await Teams.bulkCreate(driverTeams);
+          return teams;
+           
+        } catch (error) {
+          throw new Error({ error: error.message });
+        }
+      }
+      return dbTeams;
+    } catch (error) {
+        throw new Error({ error: error.message });      
+    }
+  }
+}
+
+module.exports = new TeamsService();
